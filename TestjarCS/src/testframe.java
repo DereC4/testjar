72cbbe82f34d931578a040303a89ec66db65d80e
noi01 <noi01@WINDOWS-PBTPHFR>
2020-04-14 15:31:41 -0500

    stuff

--- TestjarCS/src/testframe.java
+++ TestjarCS/src/testframe.java
@@ -1,13 +1,17 @@
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 public class testframe extends JFrame implements MouseListener
 {
 	public static final int LENGTH = 1600;
 	public static final int HEIGHT = 1000;
-	
 
 	public testframe() throws IOException
 	{
@@ -19,10 +23,21 @@ public class testframe extends JFrame implements MouseListener
 		setBounds(150, 25, LENGTH, HEIGHT);
 		addMouseListener(this);
 	}
-	public static void main(String[] args) throws IOException
+	public void paint(Graphics g)
 	{
-		testframe frame = new testframe();
-		frame.setVisible(true);
+		try
+		{
+			int ok = (int) Math.floor(Math.random() * (11) + 90);
+			BufferedImage a, b, c, d;
+			if(ok == 6)
+				a = ImageIO.read(new File("images\\assets\\coin.png"));
+			b = ImageIO.read(new File("images\\assets\\victory1.png"));
+			c = ImageIO.read(new File("images\\assets\\victoryminus1.png"));
+		}
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 	@Override
 	public void mouseClicked(MouseEvent arg0)
@@ -49,9 +64,17 @@ public class testframe extends JFrame implements MouseListener
 
 	}
 	@Override
-	public void mouseReleased(MouseEvent arg0)
+	public void mouseReleased(MouseEvent event)
 	{
-		// TODO Auto-generated method stub
-
+		if (event.getX() < 1100 && event.getY() < 200 && event.getX() > 1000 && event.getY() > 100) // shows card window
+		{
+			
+		}
+		repaint();
+	}	
+	public static void main(String[] args) throws IOException
+	{
+		testframe frame = new testframe();
+		frame.setVisible(true);
 	}
 }
